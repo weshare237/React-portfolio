ff8f620e93a9ac38381917fa14fdc2470e12bc3e
weshare237 <fopaduclair2000@gmail.com>
2022-04-03 19:37:43 +0100

    Started designing the sidebar

---
+++ src/components/Layout/index.js
@@ -0,0 +1,14 @@
+import './index.scss'
+
+import React from 'react'
+import Sidebar from '../Sidebar'
+
+const Layout = () => {
+  return (
+    <>
+      <Sidebar />
+    </>
+  )
+}
+
+export default Layout
