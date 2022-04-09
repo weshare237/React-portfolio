eb881d6376359e2ea58b142adac67505866edee1
weshare237 <fopaduclair2000@gmail.com>
2022-04-09 20:27:58 +0100

    Letters animation

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -9,14 +9,14 @@ const Layout = () => {
     <div className='App'>
       <Sidebar />
       <div className='page'>
-        <span className='tags top-tags'>&lt;body&gt;</span>
+        <span className='tags top-tags'>&lt;fopa&gt;</span>
 
         <Outlet />
 
         <span className='tags bottom-tags'>
-          &lt;/body&gt;
+          &lt;/fopa&gt;
           <br />
-          <span className='bottom-tag-html'>&lt;/html&gt;</span>
+          <span className='bottom-tag-html'>&lt;/duclair&gt;</span>
         </span>
       </div>
     </div>
