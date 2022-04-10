d22209bd16557141e24e98954b51d51469064f3f
weshare237 <fopaduclair2000@gmail.com>
2022-04-10 13:17:59 +0100

    Designed about page talking of myself

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
 import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-s.png'
 import AnimatedLetters from '../AnimatedLetters'
@@ -49,34 +50,37 @@ const Home = () => {
   }, [])
 
   return (
-    <div className='container home-page'>
-      <div className='text-zone'>
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m</span>
-          {/* <img src={LogoTitle} alt='developer' /> */}
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15}
-          />
-          <br />
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={22}
-          />
-        </h1>
-        <h2>FullStack Developer / Instructor</h2>
-        <Link to='/contact' className='flat-button'>
-          CONTACT ME
-        </Link>
+    <>
+      <div className='container home-page'>
+        <div className='text-zone'>
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            {/* <img src={LogoTitle} alt='developer' /> */}
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>FullStack Developer / Instructor</h2>
+          <Link to='/contact' className='flat-button'>
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
+      <Loader type='pacman' />
+    </>
   )
 }
 
